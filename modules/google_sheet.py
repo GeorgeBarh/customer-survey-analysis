76bf672c827e8f392aad6f296d7ce0b3499dfc17
G.Barchabas <georgebarhabas@gmail.com>
2024-09-13 13:11:37 +0300

    First step of refractoring the code in classes. Create GoogleSheet Class

---
+++ modules/google_sheet.py
@@ -0,0 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+class GoogleSheet:
+    """
+    Handles Google Sheets API authorization and worksheet access.
+    """
+    def __init__(self, sheet_name):
+        # Initialize Google Sheets API
+        scope = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive"
+        ]
+        creds = Credentials.from_service_account_file('creds.json')
+        scoped_creds = creds.with_scopes(scope)
+        gspread_client = gspread.authorize(scoped_creds)
+        self.sheet = gspread_client.open(sheet_name)
+
+    def get_worksheet(self, worksheet_name):
+        """
+        returns the worksheet object by name.
+        """
+        return self.sheet.worksheet(worksheet_name)
