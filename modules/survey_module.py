f3a34cf6638e2dda8285bcc47d9669ac3a7a0c5b
G.Barchabas <georgebarhabas@gmail.com>
2024-09-13 23:14:52 +0300

    refractor(survey_module): refractor the survey module

--- modules/survey_module.py
+++ modules/survey_module.py
@@ -7,7 +7,7 @@ class Survey:
 
     def get_customer_answers(self):
         """
-        collect customer survey responses and assign a new customer id
+        Collect customer survey responses and assign a new customer ID.
         """
         print("\nPlease rate your customer experience in the next four questions.")
         print("Data must be a number from 1-5 based on the following:")
@@ -17,10 +17,10 @@ class Survey:
         3 - Neutral experience
         4 - Good experience
         5 - Best experience
-              """)
+        """)
 
-        responses = [] 
-        last_customer_id = self.get_last_customer_id()  # Get the last customer id
+        responses = []
+        last_customer_id = self.get_last_customer_id()  # Get the last customer ID
         current_customer_id = last_customer_id + 1  # Increment for the new customer
 
         questions = [
@@ -30,17 +30,17 @@ class Survey:
             "How likely are you to recommend our product/service to a friend or colleague? (1-5): \n"
         ]
 
-        responses.append(current_customer_id)  # Add the customer id
+        responses.append(current_customer_id)  # Add the customer ID
 
         for question in questions:
             while True:
-                response = input(question)  
+                response = input(question)
                 try:
                     validated_response = self.validate_response(response)  # Validate input
                     responses.append(validated_response)  # Add validated response
                     break
                 except ValueError as e:
-                    print(e)  
+                    print(e)
 
         return responses
 
@@ -50,7 +50,7 @@ class Survey:
         """
         try:
             response = int(response)
-            if response >= 1 and response <= 5:
+            if 1 <= response <= 5:
                 return response
             else:
                 raise ValueError("Number must be between 1 and 5.")
@@ -59,14 +59,14 @@ class Survey:
 
     def get_last_customer_id(self):
         """
-        etrieve the last customer ID from the survey worksheet.
+        Retrieve the last customer ID from the survey worksheet.
         """
         data = self.sheet.get_all_values()
         if len(data) > 1:
             last_row = data[-1]
-            last_customer_id = int(last_row[0])  # customers id is in the first column
+            last_customer_id = int(last_row[0])  # Customer ID is in the first column
         else:
-            last_customer_id = 1  # Start with 1 if no data
+            last_customer_id = 0  # Start with 1 if no data
         return last_customer_id
 
     def update_survey_worksheet(self, data):
